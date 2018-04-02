cc7b9f2453b0713885b849258957986f74ef708d
jeff plata <jeffplata@uyahoo.com>
2018-04-02 21:29:55 +0800

    ch 2: mail sent but cd.to no value

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,10 @@ from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.views.generic import ListView
 
+
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
@@ -21,7 +25,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         # if page is out of range deliver last page of results
-        posts = paginator.page(paginaotr.num_pages)
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
@@ -44,3 +48,28 @@ def post_detail(request, year, month, day, post):
                   'blog/post/detail.html',
                   {'post': post})                  
 
+def post_share(request, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                                           post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(
+                cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(
+                post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']]) 
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+                  {'post': post,
+                   'form': form,
+                   'sent': sent})
--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jeffplata@gmail.com'
+EMAIL_HOST_PASSWORD = 'Hybrid13gm!!'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
